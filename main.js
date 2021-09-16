a07f405919a393b94ed45b70e196a9dd023453d9
Tamim <sktamim225@gmail.com>
2021-09-16 12:28:28 +0530

    Updated news source

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
 // targeting the news container
 let newsContainer = document.getElementById("newsContainer");
 
+// for api   
+const data = null;
+
 // Create an AJAX get Request
 const xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
 
 // Source , apiKey and keyword
-let source = "in";
-let apiKey = "59d592e1de3d41fc84d22339eee827d3";
-let keyword = "BTS";
+// let source = "in";
+// let apiKey = "59d592e1de3d41fc84d22339eee827d3";
+// let keyword = "BTS";
 
 // Send the GET request
 xhr.open(
   "GET",
-  `https://newsapi.org/v2/everything?q=${keyword}&apiKey=${apiKey}`,
+  `https://free-news.p.rapidapi.com/v1/search?q=bts&lang=en`,
   true
 );
+xhr.setRequestHeader("x-rapidapi-host", "free-news.p.rapidapi.com");
+xhr.setRequestHeader("x-rapidapi-key", "e4c04caea2msh200bf9b846168a4p1809b2jsnb8543938d384");
+
 xhr.onload = function () {
   if (this.status == 200) {
     let json = JSON.parse(this.responseText);
@@ -26,13 +33,13 @@ xhr.onload = function () {
       let newsHtml = `
       <div class="col-lg-3 g-3 my-3">
             <div class="col">
-                <img class="img-fluid border border-success" src="${element.urlToImage}" alt="Image">
+                <img class="img-fluid border border-success" src="${element.media}" alt="Image">
             </div>
             <div class="col my-2 py-2">
                 <h5 class="m-0">${element.title}</h5>
             </div>
             <div class="col">
-                <a href="${element.url}" target="_blank" class="btn btn-success w-100">Show more</a>
+                <a href="${element.link}" target="_blank" class="btn btn-success w-100">Show more</a>
             </div>
       </div>`;
         news += newsHtml;
@@ -43,4 +50,4 @@ xhr.onload = function () {
     console.log("An error occurred");
   }
 };
-xhr.send();
+xhr.send(data);
